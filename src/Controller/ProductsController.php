d001374bc32fc71a25827b0376cd4a955c0c2772
Nath <taleonnathaniel@gmail.com>
2025-10-01 19:09:29 +0800

    Added Products entity

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,88 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Form\ProductsType;
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/products')]
 final class ProductsController extends AbstractController
 {
-    #[Route('/products', name: 'app_products_index')]
-    public function index(): Response
+    #[Route(name: 'app_products_index', methods: ['GET'])]
+    public function index(ProductsRepository $productsRepository): Response
     {
-        $products = [
-            [
-                'name' => 'Garden Glow',
-                'price' => 199.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/GardenGlow.jpg',
-            ],
-            [
-                'name' => 'Mediterranean Feta Feast',
-                'price' => 229.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/MediterraneanFetaFeast.jpg',
-            ],
-            [
-                'name' => 'Global Grain Bowl',
-                'price' => 249.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/GlobalGrainBowl.jpg',
-            ],
-            [
-                'name' => 'Summer Berry Power Bowl',
-                'price' => 219.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/SummerBerryPowerBowl.jpg',
-            ],
-            [
-                'name' => 'Harvest Bowl',
-                'price' => 199.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/HarvestBowl.jpg',
-            ],
-            [
-                'name' => 'Caprese Avocado Delight',
-                'price' => 239.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/CapreseAvocadoDelight.jpg',
-            ],
-            [
-                'name' => 'Classic Avocado',
-                'price' => 189.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/ClassisAvocado.jpg',
-            ],
-            [
-                'name' => 'Mediterranean Bliss',
-                'price' => 229.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/MaditerraneanBlissSalad.jpg',
-            ],
-            [
-                'name' => 'Rainbow Fresh Salad',
-                'price' => 209.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/RainbowFreshSalad.jpg',
-            ],
-            [
-                'name' => 'Sunshine Fruit & Veggie Bowl',
-                'price' => 219.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/SunshineFruit&VeggieBowl.jpg',
-            ],
-            [
-                'name' => 'Vitality Crunch Bowl',
-                'price' => 229.00,
-                'unit' => 'per bowl',
-                'image' => 'imageeee/VitalityCurnchBowl.jpg',
-            ],
-        ];
-
         return $this->render('products/index.html.twig', [
-            'products' => $products,
+            'products' => $productsRepository->findAll(),
         ]);
     }
+
+    #[Route('/new', name: 'app_products_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Products();
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('products/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_products_show', methods: ['GET'])]
+    public function show(Products $product): Response
+    {
+        return $this->render('products/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_products_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Products $product, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('products/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_products_delete', methods: ['POST'])]
+    public function delete(Request $request, Products $product, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
-
-
